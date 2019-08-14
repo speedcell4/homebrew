0a841edc46241bbfac26b558aa53920b08c77518
speedcell4 <speedcell4@gmail.com>
2019-08-14 17:00:43 +0900

    Update cdec

--- cdec.rb
+++ cdec.rb
@@ -3,19 +3,29 @@ class Cdec < Formula
   homepage "https://github.com/redpony/cdec"
   head "https://github.com/redpony/cdec.git"
 
-  depends_on "bjam" => :build
   depends_on "boost" => :build
   depends_on "flex" => :build
   depends_on "cmake" => :build
 
   def install
     system "cmake", "."
-    system "bjam", "install", "-j4", "--prefix=#{prefix}"
+    system "make", "-j4"
+
+    bin.install Dir["corpus/*.sh"]
+    bin.install Dir["corpus/*.pl"]
+    bin.install Dir["corpus/*.py"]
+    bin.install "klm/lm/build_binary"
+    bin.install "klm/lm/ngram_query"
+    bin.install "training/mira/mira.py"
+    bin.install "word-aligner/fast_align"
+    bin.install "utils/atools"
+    bin.install "extractor/sacompile"
+    bin.install "extractor/extract"
   end
 
   def test
     system "make", "test"
-    system "perl", "tests/run-system-tests.pl"
+    # system "perl", "tests/run-system-tests.pl"
   end
 
 end
